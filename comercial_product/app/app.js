2c0b8a8728cd7c789eae556a3305d69f03ff4691
Tony-404 <chernani2003@gmail.com>
2021-08-22 19:09:29 -0500

    comercial

---
+++ comercial_product/app/app.js
@@ -0,0 +1,37 @@
+import { main } from "./module/hero.js"
+let alert = document.getElementById('alert')
+let content = document.getElementById('content')
+content.innerHTML = main
+
+function conseguirdatos(){
+    let email = document.getElementById('email').value
+    let country = document.getElementById('select_country').value
+    let direction = document.getElementById('direction').value
+    if(email === "" || country === "" || direction === ""){
+        alert.innerHTML = 
+        `
+        <div class="notification has-text-black">
+            <button class="delete" onclick="borrarAlert()"></button>
+            The fields are not valid, please try again
+        </div>  
+        `
+    }
+    else{
+        alert.innerHTML = `
+        <div class="notification has-text-black">
+            <button class="delete" onclick="borrarAlert()"></button>
+            <strong>Congratulations</strong><br>
+            Our logistics operator will send the product to the address: ${direction}<br>
+            Which is in: ${country}<br>
+            Check the span area in your mail ${email} to know the details of your product.
+            .
+        </div>  
+        `
+    }
+}
+
+function borrarAlert(){
+    alert.innerHTML =''
+}
+
+document.querySelector('#btn_buy_top').addEventListener('click', conseguirdatos)
