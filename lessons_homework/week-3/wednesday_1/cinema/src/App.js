0ab40d03646e018ce507966ff8bf6405d0fe48d7
Tony-404 <chernani2003@gmail.com>
2021-09-01 09:38:50 -0500

    documents

---
+++ lessons_homework/week-3/wednesday_1/cinema/src/App.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const PruebaH1 = styled.h1`
+color:red;
+background-color: blue;
+`
+
+export default function App(){
+    return(
+        <PruebaH1>Hola Lola</PruebaH1>
+    )
+}
